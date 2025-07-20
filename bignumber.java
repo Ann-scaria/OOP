aa5cd4b661c00d12fe4571054df4128e7b01ae79
Ann Scaria <annscariathottakkara@gmail.com>
2025-07-20 12:09:01 +0530

    bignumber.java

---
+++ bignumber.java
@@ -0,0 +1,9 @@
+public class ternaryOperatorDemo{
+    public static void main(String[]args){
+        int firstNumber=1;
+        int secondNumber=2;
+        int bigNumber;
+        bigNumber=firstNumber>secondNumber?firstNumber:secondNumber;
+        System.out.println(bigNumber+" "+"is the bigNumber");
+    }
+}
